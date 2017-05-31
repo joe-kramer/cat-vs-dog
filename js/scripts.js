fa1908fcf42c212c81f46642bc86a49fd9879cac
Joe Kramer <joekramer1125@gmail.com>
2017-05-31 10:54:40 -0700

    Add jquery for buttons. functionality pushes li element into ul and has clickable removal

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
   $("#dogBtn").click(function() {
-    $("#dog").append(<li>BARK!</li>);
-  })
-})
+    $("#dog").append("<li>BARK!</li>");
+    $("#cat").append("<li>Hiss!</li>");
+    $("#dog").children("li").last().click(function() {
+      $(this).remove();
+    });
+    $("#cat").children("li").last().click(function() {
+      $(this).remove();
+    });
+  });
+  $("#catBtn").click(function() {
+    $("#dog").append("<li>bark bark bark</li>");
+    $("#cat").append("<li>Meow!</li>");
+    $("#dog").children("li").last().click(function() {
+      $(this).remove();
+    });
+    $("#cat").children("li").last().click(function() {
+      $(this).remove();
+    });
+  });
+  $("#clearBtn").click(function() {
+    $("#dog").empty();
+    $("#cat").empty();
+  });
+});
